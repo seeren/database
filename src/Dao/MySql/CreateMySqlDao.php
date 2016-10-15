aae4892fdbc2df1fe5f8cb92b89128db521e0bbd
Cyril <consultant@seeren.fr>
2016-10-15 22:51:09 +0200

    Create create dao

--- src/Dao/MySql/CreateMySqlDao.php
+++ src/Dao/MySql/CreateMySqlDao.php
@@ -1,15 +1,21 @@
 <?php
 
 /**
- * This file contain CreateMySqlDao class
+ * This file contain Seeren\Database\Dao\MySql\CreateMySqlDao class
+ *     __
+ *    / /__ __ __ __ __ __
+ *   / // // // // // // /
+ *  /_// // // // // // /
+ *    /_//_//_//_//_//_/
  *
- * @package Database
+ * @copyright (c) Cyril Ichti <consultant@seeren.fr>
+ * @link http://www.seeren.fr/ Seeren
+ * @version 1.0.1
  */
 
 namespace Seeren\Database\Dao\MySql;
 
-use Seeren\Database\Dao\DaoInterface;
-use Seeren\Database\Dao\MySql\AbstractMySqlDao;
+use Seeren\Database\Dao\AbstractDao;
 use Seeren\Database\Dao\MySql\MySqlDaoInterface;
 use Seeren\Database\Dal\DalInterface;
 use Seeren\Database\Table\TableInterface;
@@ -21,12 +27,8 @@ use Seeren\Database\Table\Key\KeyInterface;
  * @category Seeren
  * @package Database
  * @subpackage Dao\MySql
- * @author Cyril
- * @copyright 2016
- * @version 1.0.1
- * @final
  */
-final class CreateMySqlDao extends AbstractMySqlDao implements MySqlDaoInterface
+class CreateMySqlDao extends AbstractDao implements MySqlDaoInterface
 {
 
     /**
@@ -34,64 +36,9 @@ final class CreateMySqlDao extends AbstractMySqlDao implements MySqlDaoInterface
      *
      * @return null
      */
-    final public function __construct()
+    public function __construct()
     {
         parent::__construct();
-    }
-
-
-    /**
-     * Execute operation
-     *
-     * @param TableInterface $table table
-     * @param DalInterface $dal access layer
-     * @return DaoInterface self
-     */
-    final protected function execute(
-        TableInterface $table,
-        DalInterface $dal): DaoInterface
-    {
-        $dal->getLayer()->query($this->queryString);
-        $this->row++;
-        return $this;
-    }
-
-    /**
-     * Query table for dal
-     *
-     * @param TableInterface $table table
-     * @param DalInterface $dal access layer
-     * @return DaoInterface self
-     */
-    final public function query(
-        TableInterface $table,
-        DalInterface $dal): DaoInterface
-    {
-        return parent::query($table, $dal)->execute($table, $dal);
-    }
-
-    /**
-     * Get MySql syntaxe
-     *
-     * @param TableInterface $table table
-     * @return string MySql operation for table
-     */
-    final public function mySql(TableInterface $table): string
-    {
-        $mySql = "CREATE TABLE `" . $table::NAME . "` (\n";
-        foreach ($table->get($table::ATTR_COLUMN) as &$column) {
-            $mySql .= "`" . $column->getName() . "` "
-                   . $this->constant($column->getType())
-                   . "(" . $column->getSize() . ")";
-            foreach ($column->getOption() as &$option) {
-                $mySql .= " " . $this->constant($option);
-            }
-            $mySql .= ",\n";
-        }
-        return substr($mySql, 0, -2) . "\n)"
-             . " ENGINE=" . $this->constant($table->get($table::ATTR_ENGINE))
-             . " CHARSET=utf8;\n"
-             . $this->constraint($table);
     }
 
     /**
@@ -100,25 +47,25 @@ final class CreateMySqlDao extends AbstractMySqlDao implements MySqlDaoInterface
      * @param TableInterface $table table
      * @return string constraint syntaxe
      */
-    final private function constraint(TableInterface $table): string
+    private function constraint(TableInterface $table): string
     {
         $mySql = $sk = $fk = "";
-        foreach ($table->get($table::ATTR_KEY) as $key => &$value) {
+        foreach ($table->get($table::ATTR_KEY) as $key => $value) {
             if (KeyInterface::FOREIGN !== $value->getType()) {
                 $sk .= $this->addKey($value, $key);
             } else {
                 $fk .= $this->addForeign($value, $key);
             }
         }
-        foreach ([$sk, $fk] as &$value) {
+        foreach ([$sk, $fk] as $value) {
             $mySql .= "" !== $value
-                    ? "ALTER TABLE `" . $table::NAME . "`\n"
+                    ? "ALTER TABLE `" . $table->get($table::ATTR_NAME) . "`\n"
                     . substr($value, 0, -2) . ";\n"
                     : "";
         }
         return substr($mySql, 0, -1);
     }
-
+    
     /**
      * Get add key syntaxe
      *
@@ -126,13 +73,13 @@ final class CreateMySqlDao extends AbstractMySqlDao implements MySqlDaoInterface
      * @param string $name name
      * @return string add key syntaxe
      */
-    final private function addKey(KeyInterface $key, string $name): string
+    private function addKey(KeyInterface $key, string $name): string
     {
         return "ADD " . $this->constant($key->getType())
              . " `k_" . $name . "`"
              . " (`" . implode("`, `", ($key->getSubject())) . "`),\n";
     }
-
+    
     /**
      * Get add foreign key syntaxe
      *
@@ -140,13 +87,50 @@ final class CreateMySqlDao extends AbstractMySqlDao implements MySqlDaoInterface
      * @param string $name name
      * @return string add foreign key syntaxe
      */
-    final private function addForeign(KeyInterface $key, string $name): string
+    private function addForeign(KeyInterface $key, string $name): string
     {
         return "ADD CONSTRAINT" . " `fk_" . $name . "`"
              . " " . $this->constant($key->getType())
              . " (`" . implode("`, `", ($key->getSubject())) . "`) "
              . " REFERENCES `" . $key->getForeigner() . "`"
              . " (`" . implode("`, `", ($key->getForeignerSubject())) . "`),\n";
+    }
+
+    /**
+     * Template method Get MSql syntaxe
+     *
+     * @param TableInterface $table table
+     * @return string Myql operation for table
+     */
+    protected function getSyntax(TableInterface $table): string
+    {
+        $mySql = "CREATE TABLE IF NOT EXISTS `"
+                . $table->get($table::ATTR_NAME) . "` (\n";
+        foreach ($table->get($table::ATTR_COLUMN) as $column) {
+            $mySql .= "`" . $column->getName() . "` "
+                   . $this->constant($column->getType())
+                   . "(" . $column->getSize() . ")";
+            foreach ($column->getOption() as $option) {
+                $mySql .= " " . $this->constant($option);
+            }
+            $mySql .= ",\n";
+        }
+        return substr($mySql, 0, -2) . "\n)"
+             . " ENGINE=innoDB CHARSET=utf8;\n"
+             . $this->constraint($table);
+    }
+
+    /**
+     * Execute operation
+     *
+     * @param TableInterface $table table
+     * @param DalInterface $dal access layer
+     * @return null
+     */
+    protected function execute(TableInterface $table, DalInterface $dal)
+    {
+        $dal->getLayer()->query($this->queryString);
+        $this->row++;
     }
 
 }
