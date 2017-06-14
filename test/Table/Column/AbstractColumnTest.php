03fbf960ed85391129a1055bc58dfc0ad539a09c
Cyril <consultant@seeren.fr>
2017-06-14 16:52:12 +0200

    Build abstract column test

--- test/Table/Column/AbstractColumnTest.php
+++ test/Table/Column/AbstractColumnTest.php
@@ -35,4 +35,47 @@ abstract class AbstractColumnTest extends \PHPUnit\Framework\TestCase
     */
    abstract protected function getColumn(): ColumnInterface;
 
+   /**
+    * Test get name
+    */
+   abstract public function testGetName();
+
+   /**
+    * Test get type
+    */
+   abstract public function testGetType();
+
+   /**
+    * Test get type default
+    */
+   abstract public function testGetTypeDefault();
+
+   /**
+    * Test get size
+    */
+   abstract public function testGetSize();
+
+   /**
+    * Test get option
+    */
+   abstract public function testGetOption();
+
+   /**
+    * Test set value
+    */
+   abstract public function testSetValue();
+
+   /**
+    * Test get value param
+    */
+   abstract public function testGetParam();
+
+   /**
+    * Assert get option defaukt
+    */
+    protected function testGetOptionDefault()
+    {
+      $this->assertTrue($this->getColumn()->getOption() === []);
+    }
+
 }
--- test/Table/Column/StringColumnTest.php
+++ test/Table/Column/StringColumnTest.php
@@ -16,12 +16,13 @@
 namespace Seeren\Database\Test;
 
 use Seeren\Database\Table\Column\ColumnInterface;
-use Seeren\Database\Table\Column\Column;
-use ReflectionClass;
 use Seeren\Database\Table\Column\StringColumn;
+use Seeren\Database\Table\Column\StringColumnInterface;
+use ReflectionClass;
+use PDO;
 
 /**
- * Class for test Column
+ * Class for test StringColumn
  * 
  * @category Seeren
  * @package Database
@@ -38,15 +39,111 @@ class StringColumnTest extends AbstractColumnTest
    protected function getColumn(): ColumnInterface
    {
        return (new ReflectionClass(StringColumn::class))->newInstanceArgs([
-           "rr",
            "column_name",
-           11
+           StringColumnInterface::CHAR,
+           255,
        ]);
    }
 
-   public function testFoo()
+   /**
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getName
+    */
+   public function testGetName()
    {
-       $this->getColumn();
-       $this->assertTrue(true);
+       $this->assertTrue($this->getColumn()->getName() === "column_name");
    }
+
+   /**
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getType
+    */
+   public function testGetType()
+   {
+       $this->assertTrue($this->getColumn()->getType() === "CHAR");
+   }
+
+   /**
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getType
+    */
+   public function testGetTypeDefault()
+   {
+       $column = $this->getColumn();
+       $column->__construct(
+           "column_name",
+           "bad type",
+           255);
+       $this->assertTrue($this->getColumn()->getType() === "CHAR");
+   }
+
+   /**
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getSize
+    */
+   public function testGetSize()
+   {
+       $this->assertTrue($this->getColumn()->getSize() === 255);
+   }
+
+   /**
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getOption
+    */
+   public function testGetOption()
+   {
+       $column = $this->getColumn();
+       $column->__construct(
+           "column_name",
+           StringColumnInterface::CHAR,
+           255,
+           [
+               StringColumnInterface::OPT_DEFAULT_STRING,
+               "bad option",
+               StringColumnInterface::OPT_NOT_NULL
+           ]);
+       $this->assertTrue($column->getOption() === [
+           StringColumnInterface::OPT_DEFAULT_STRING,
+           StringColumnInterface::OPT_NOT_NULL
+       ]);
+   }
+
+   /**
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getOption
+    */
+   public function testGetOptionDefault()
+   {
+       parent::testGetOptionDefault();
+   }
+
+   /**
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\StringColumn::setValue
+    * @covers \Seeren\Database\Table\Column\StringColumn::getValue
+    */
+   public function testSetValue()
+   {
+       $column = $this->getColumn();
+       $column->setValue(null);
+       $this->assertTrue($column->getValue() === "");
+   }
+
+   /**
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\StringColumn::getParam
+    */
+   public function testGetParam()
+   {
+       $this->assertTrue($this->getColumn()->getParam() === PDO::PARAM_STR);
+   }
+
 }
