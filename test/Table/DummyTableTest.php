d4b5ee128dfbf4ebe256ebe5d4ce88703449d3fa
Cyril <consultant@seeren.fr>
2017-06-16 23:53:26 +0200

    Add master table test

---
+++ test/Table/DummyTableTest.php
@@ -0,0 +1,262 @@
+<?php
+
+/**
+ * This file contain Seeren\Database\Test\Test\Table\DummyTableTest class
+ *     __
+ *    / /__ __ __ __ __ __
+ *   / // // // // // // /
+ *  /_// // // // // // /
+ *    /_//_//_//_//_//_/
+ *
+ * @copyright (c) Cyril Ichti <consultant@seeren.fr>
+ * @link https://github.com/seeren/database
+ * @version 1.0.1
+ */
+
+namespace Seeren\Database\Test\Table;
+
+use Seeren\Database\Table\TableInterface;
+use ReflectionClass;
+
+/**
+ * Class for test DummyTable
+ * 
+ * @category Seeren
+ * @package Database
+ * @subpackage Test\Table
+ */
+class DummyTableTest extends AbstractTableTest
+{
+
+    /**
+     * Get Table
+     *
+     * @return TableInterface table
+     */
+    protected function getTable(): TableInterface
+   {
+       return (new ReflectionClass(DummyTable::class))->newInstanceArgs([]);
+   }
+
+   /**
+    * @covers \Seeren\Database\Test\Table\DummyTable::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::__get
+    * @covers \Seeren\Database\Table\AbstractTable::__set
+    * @covers \Seeren\Database\Table\AbstractTable::addColumn
+    * @covers \Seeren\Database\Table\AbstractTable::addKey
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getName
+    * @covers \Seeren\Database\Table\Column\IntegerColumn::__construct
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Key\Key::__construct
+    * @covers \Seeren\Database\Table\MasterTable::__construct
+    * @covers \Seeren\Database\Table\MasterTable::__get
+    * @covers \Seeren\Database\Table\MasterTable::__set
+    */
+   public function test__get__setNull()
+   {
+       parent::test__get__setNull();
+   }
+
+   /**
+    * @covers \Seeren\Database\Test\Table\DummyTable::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::__get
+    * @covers \Seeren\Database\Table\AbstractTable::__set
+    * @covers \Seeren\Database\Table\AbstractTable::addColumn
+    * @covers \Seeren\Database\Table\AbstractTable::addKey
+    * @covers \Seeren\Database\Table\AbstractTable::get
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getName
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getValue
+    * @covers \Seeren\Database\Table\Column\IntegerColumn::__construct
+    * @covers \Seeren\Database\Table\Column\IntegerColumn::setValue
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Key\Key::__construct
+    * @covers \Seeren\Database\Table\MasterTable::__construct
+    * @covers \Seeren\Database\Table\MasterTable::__get
+    * @covers \Seeren\Database\Table\MasterTable::__set
+    * @covers \Seeren\Database\Table\MasterTable::get
+    */
+   public function test__get__set()
+   {
+       parent::test__get__set();
+   }
+   
+   /**
+    * @covers \Seeren\Database\Test\Table\DummyTable::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::addColumn
+    * @covers \Seeren\Database\Table\AbstractTable::addKey
+    * @covers \Seeren\Database\Table\AbstractTable::get
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getName
+    * @covers \Seeren\Database\Table\Column\IntegerColumn::__construct
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Key\Key::__construct
+    * @covers \Seeren\Database\Table\MasterTable::__construct
+    * @covers \Seeren\Database\Table\MasterTable::get
+    */
+   public function testGet()
+   {
+       parent::testGet();
+   }
+
+   /**
+    * @covers \Seeren\Database\Test\Table\DummyTable::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::addClause
+    * @covers \Seeren\Database\Table\AbstractTable::addColumn
+    * @covers \Seeren\Database\Table\AbstractTable::addKey
+    * @covers \Seeren\Database\Table\AbstractTable::get
+    * @covers \Seeren\Database\Table\Clause\Clause::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getName
+    * @covers \Seeren\Database\Table\Column\IntegerColumn::__construct
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Key\Key::__construct
+    * @covers \Seeren\Database\Table\MasterTable::__construct
+    * @covers \Seeren\Database\Table\MasterTable::addClause
+    * @covers \Seeren\Database\Table\MasterTable::get
+    */
+   public function testAddClause()
+   {
+       parent::testAddClause();
+   }
+
+   /**
+    * @covers \Seeren\Database\Test\Table\DummyTable::__construct
+    * @covers \Seeren\Database\Dao\AbstractDao::__construct
+    * @covers \Seeren\Database\Dao\AbstractDao::close
+    * @covers \Seeren\Database\Dao\MySql\OpenMySqlDao::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::addColumn
+    * @covers \Seeren\Database\Table\AbstractTable::addKey
+    * @covers \Seeren\Database\Table\AbstractTable::get
+    * @covers \Seeren\Database\Table\AbstractTable::set
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getName
+    * @covers \Seeren\Database\Table\Column\IntegerColumn::__construct
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Key\Key::__construct
+    * @covers \Seeren\Database\Table\MasterTable::__construct
+    * @covers \Seeren\Database\Table\MasterTable::get
+    */
+   public function testSet()
+   {
+       parent::testSet();
+   }
+
+   /**
+    * @covers \Seeren\Database\Test\Table\DummyTable::__construct
+    * @covers \Seeren\Database\Dao\AbstractDao::__construct
+    * @covers \Seeren\Database\Dao\AbstractDao::close
+    * @covers \Seeren\Database\Dao\MySql\OpenMySqlDao::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::addClause
+    * @covers \Seeren\Database\Table\AbstractTable::addColumn
+    * @covers \Seeren\Database\Table\AbstractTable::addKey
+    * @covers \Seeren\Database\Table\AbstractTable::clear
+    * @covers \Seeren\Database\Table\AbstractTable::get
+    * @covers \Seeren\Database\Table\AbstractTable::set
+    * @covers \Seeren\Database\Table\Clause\Clause::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getName
+    * @covers \Seeren\Database\Table\Column\IntegerColumn::__construct
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Key\Key::__construct
+    * @covers \Seeren\Database\Table\MasterTable::__construct
+    * @covers \Seeren\Database\Table\MasterTable::addClause
+    * @covers \Seeren\Database\Table\MasterTable::clear
+    * @covers \Seeren\Database\Table\MasterTable::get
+    */
+   public function testClear()
+   {
+       parent::testClear();
+   }
+
+   /**
+    * @covers \Seeren\Database\Test\Table\DummyTable::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::__call
+    * @covers \Seeren\Database\Table\MasterTable::__call
+    * @covers \Seeren\Database\Table\AbstractTable::addColumn
+    * @covers \Seeren\Database\Table\AbstractTable::addKey
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getName
+    * @covers \Seeren\Database\Table\Column\IntegerColumn::__construct
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Key\Key::__construct
+    * @covers \Seeren\Database\Table\MasterTable::__construct
+    * @expectedException \InvalidArgumentException
+    */
+   public function test__callInvalidArgumentException()
+   {
+       parent::test__callInvalidArgumentException();
+   }
+
+   /**
+    * @covers \Seeren\Database\Test\Table\DummyTable::__construct
+    * @covers \Seeren\Database\Dal\Dal::__construct
+    * @covers \Seeren\Database\Dal\Dal::getLayer
+    * @covers \Seeren\Database\Dal\MySql\MySqlDal::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::addColumn
+    * @covers \Seeren\Database\Table\AbstractTable::addKey
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getName
+    * @covers \Seeren\Database\Table\Column\IntegerColumn::__construct
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Key\Key::__construct
+    * @covers \Seeren\Database\Table\MasterTable::__call
+    * @covers \Seeren\Database\Table\MasterTable::__construct
+    * @expectedException \RuntimeException
+    */
+   public function test__callRuntimeException()
+   {
+       parent::test__callRuntimeException();
+   }
+
+   /**
+    * @covers \Seeren\Database\Test\Table\DummyTable::__construct
+    * @covers \Seeren\Database\Dal\Dal::__construct
+    * @covers \Seeren\Database\Dal\Dal::getLayer
+    * @covers \Seeren\Database\Dal\Dal::getObject
+    * @covers \Seeren\Database\Dal\Dal::query
+    * @covers \Seeren\Database\Dal\Dal::setLayer
+    * @covers \Seeren\Database\Dal\MySql\MySqlDal::__construct
+    * @covers \Seeren\Database\Dao\AbstractDao::__construct
+    * @covers \Seeren\Database\Dao\AbstractDao::clone
+    * @covers \Seeren\Database\Dao\AbstractDao::close
+    * @covers \Seeren\Database\Dao\AbstractDao::query
+    * @covers \Seeren\Database\Dao\MySql\AbstractMySqlDao::__construct
+    * @covers \Seeren\Database\Dao\MySql\AbstractMySqlDao::bindParam
+    * @covers \Seeren\Database\Dao\MySql\AbstractMySqlDao::close
+    * @covers \Seeren\Database\Dao\MySql\AbstractMySqlDao::getClause
+    * @covers \Seeren\Database\Dao\MySql\SelectMySqlDao::__construct
+    * @covers \Seeren\Database\Dao\MySql\SelectMySqlDao::execute
+    * @covers \Seeren\Database\Dao\MySql\SelectMySqlDao::getSyntax
+    * @covers \Seeren\Database\Table\AbstractTable::__construct
+    * @covers \Seeren\Database\Table\AbstractTable::__set
+    * @covers \Seeren\Database\Table\AbstractTable::addColumn
+    * @covers \Seeren\Database\Table\AbstractTable::addKey
+    * @covers \Seeren\Database\Table\AbstractTable::get
+    * @covers \Seeren\Database\Table\AbstractTable::set
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::__construct
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getName
+    * @covers \Seeren\Database\Table\Column\AbstractColumn::getValue
+    * @covers \Seeren\Database\Table\Column\IntegerColumn::__construct
+    * @covers \Seeren\Database\Table\Column\StringColumn::__construct
+    * @covers \Seeren\Database\Table\Key\Key::__construct
+    * @covers \Seeren\Database\Table\MasterTable::__construct
+    * @covers \Seeren\Database\Table\MasterTable::__set
+    * @covers \Seeren\Database\Table\MasterTable::get
+    * @covers \Seeren\Database\Table\MasterTable::select
+    */
+   public function test__call()
+   {
+       parent::test__call();
+   }
+
+}
---
+++ test/Table/MasterTableTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file contain Seeren\Database\Test\Table\MasterTableTest class
+ *     __
+ *    / /__ __ __ __ __ __
+ *   / // // // // // // /
+ *  /_// // // // // // /
+ *    /_//_//_//_//_//_/
+ *
+ * @copyright (c) Cyril Ichti <consultant@seeren.fr>
+ * @link https://github.com/seeren/database
+ * @version 1.0.1
+ */
+
+namespace Seeren\Database\Test\Table;
+
+/**
+ * Class for test MasterTable
+ * 
+ * @category Seeren
+ * @package Database
+ * @subpackage Test\Table
+ * @abstract
+ */
+abstract class MasterTableTest extends AbstractTableTest
+{
+
+ 
+}
