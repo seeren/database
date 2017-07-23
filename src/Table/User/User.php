ba20400c9a94942f87c797c32e43083091abd51b
Cyril <consultant@seeren.fr>
2017-07-24 00:24:47 +0200

    Remove json serialize on implementation

--- src/Table/User/User.php
+++ src/Table/User/User.php
@@ -10,7 +10,7 @@
  *
  * @copyright (c) Cyril Ichti <consultant@seeren.fr>
  * @link http://www.seeren.fr/ Seeren
- * @version 1.1.1
+ * @version 1.2.1
  */
 
 namespace Seeren\Database\Table\User;
@@ -18,7 +18,6 @@ namespace Seeren\Database\Table\User;
 use Seeren\Database\Table\AbstractTable;
 use Seeren\Database\Table\Column\StringColumn;
 use Seeren\Database\Table\Key\Key;
-use JsonSerializable;
 
 /**
  * Class for represent a database user
@@ -27,7 +26,7 @@ use JsonSerializable;
  * @package Database
  * @subpackage Table/User
  */
-class User extends AbstractTable implements UserInterface, JsonSerializable
+class User extends AbstractTable implements UserInterface
 {
 
     const
@@ -69,19 +68,6 @@ class User extends AbstractTable implements UserInterface, JsonSerializable
         $this->addKey(
             new Key(Key::INDEX, [self::COL_PSWD])
         );
-    }
-
-    /**
-     * Json serialize
-     *
-     * @return array data to json encode
-     */
-    public function jsonSerialize() {
-        $json = [];
-        foreach ($this->get(self::ATTR_COLUMN) as $key => $value) {
-            $json[$key] = $value->getValue();
-        }
-        return $json;
     }
 
 }
